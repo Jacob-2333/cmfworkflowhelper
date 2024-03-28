f4e081ba33e1d3f38bf05df18cc324139dacae69
Chun Cai <ChunCai@criticalmanufacturing.com>
2024-03-28 20:00:41 +0800

    modified:   package.json 	modified:   src/extension.ts 	new file:   src/resources/dark/refresh.svg 	new file:   src/resources/light/refresh.svg

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	// vscode.window.createTreeView('nodeDependencies', {
+	// 	treeDataProvider: new NodeDependenciesProvider("F:\\VS2024\\MyVscodeExtension\\cmfworkflowhelper")
+	// });
+	const nodeDependenciesProvider = new NodeDependenciesProvider("F:\\VS2024\\MyVscodeExtension\\cmfworkflowhelper");
+	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () =>{
+		nodeDependenciesProvider.refresh();
+		console.log("my refresh")
+
+	}
+  );
+
 	// my first webview
 	// 创建一个webview面板
 	const panel = vscode.window.createWebviewPanel(
@@ -28,24 +43,49 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// 设置面板消息监听
 	panel.webview.onDidReceiveMessage(
-		message => {
+		async message => {
 			// 处理来自webview的消息
 			switch (message.command) {
 				case "showMessage":
 					vscode.window.showInformationMessage(message.text);
 					break;
 				case "listEditors": {
-					let aa= vscode.window.showTextDocument(vscode.Uri.file('E:\\VS2024\\Vscode\\testfiles\\test2.json'))
-					aa.then(textEditor=>{
-						let text=textEditor.document.getText();
-						console.log(`text: ${text}`);
-						let editors = vscode.window.visibleTextEditors;
-						console.log(editors)
-						editors.forEach(editor => {
-							console.log(editor.document);
-						});
+					// let textdoc:vscode.TextDocument=new vscode.TextDocument();
+					// 先尝试创建文件
+					// let textEditor= await vscode.window.showTextDocument(vscode.Uri.file('F:\\VS2024\\MyVscodeExtension\\testfiles\\test2.json'));
+					// let text = textEditor.document.getText();
+					// // textEditor.document
+					// console.log(`text: ${text}`);
+					// let editors = vscode.window.visibleTextEditors;
+					// console.log(editors)
+					// editors.forEach(editor => {
+					// 	console.log(editor.document);
+					// });
+					let filePath = vscode.Uri.joinPath(vscode.Uri.file('F:\\VS2024\\MyVscodeExtension\\testfiles\\test2.json'));
+					// let filePath = vscode.Uri.joinPath(vscode.Uri.parse('F:\\VS2024\\MyVscodeExtension\\testfiles\\'), 'newFile.txt');
+					try {
+						await fs.promises.writeFile(filePath.fsPath, `{"test":"test1"}`);
+						console.log(`create file successed`);
+					} catch (error: any) {
+						vscode.window.showErrorMessage(`Failed to create file: ${error.message}`);
+						return;
+					}
+					let doc = vscode.workspace.openTextDocument(filePath);
+					doc.then(document => {
+						let aa = vscode.window.showTextDocument(document);
+						aa.then(textEditor => {
+							let text = textEditor.document.getText();
+							// textEditor.document
+							console.log(`text: ${text}`);
+							let editors = vscode.window.visibleTextEditors;
+							console.log(editors)
+							editors.forEach(editor => {
+								console.log(editor.document);
+							});
 
-					})
+						})
+					});
+
 					break;
 				}
 			}
@@ -109,3 +149,103 @@ function getWebviewContent(cat: string) {
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
+
+
+export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
+	constructor(private workspaceRoot: string) { }
+
+	getTreeItem(element: Dependency): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: Dependency): Thenable<Dependency[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
+		}
+
+		if (element) {
+			return Promise.resolve(
+				this.getDepsInPackageJson(
+					path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')
+				)
+			);
+		} else {
+			const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
+			if (this.pathExists(packageJsonPath)) {
+				return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
+			} else {
+				vscode.window.showInformationMessage('Workspace has no package.json');
+				return Promise.resolve([]);
+			}
+		}
+	}
+
+	/**
+	 * Given the path to package.json, read all its dependencies and devDependencies.
+	 */
+	private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
+		if (this.pathExists(packageJsonPath)) {
+			const toDep = (moduleName: string, version: string): Dependency => {
+				if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
+					return new Dependency(
+						moduleName,
+						version,
+						vscode.TreeItemCollapsibleState.Collapsed
+					);
+				} else {
+					return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None);
+				}
+			};
+
+			const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+
+			const deps = packageJson.dependencies
+				? Object.keys(packageJson.dependencies).map(dep =>
+					toDep(dep, packageJson.dependencies[dep])
+				)
+				: [];
+			const devDeps = packageJson.devDependencies
+				? Object.keys(packageJson.devDependencies).map(dep =>
+					toDep(dep, packageJson.devDependencies[dep])
+				)
+				: [];
+			return deps.concat(devDeps);
+		} else {
+			return [];
+		}
+	}
+
+	private pathExists(p: string): boolean {
+		try {
+			fs.accessSync(p);
+		} catch (err) {
+			return false;
+		}
+		return true;
+	}
+
+	private _onDidChangeTreeData: vscode.EventEmitter<Dependency | undefined | null | void> = new vscode.EventEmitter<Dependency | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<Dependency | undefined | null | void> = this._onDidChangeTreeData.event;
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+}
+
+class Dependency extends vscode.TreeItem {
+	constructor(
+		public readonly label: string,
+		private version: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState
+	) {
+		super(label, collapsibleState);
+		this.tooltip = `${this.label}-${this.version}`;
+		this.description = this.version;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+}
